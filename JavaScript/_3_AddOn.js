2c89f5d9e45eb3831e3ff8c281b9af033cb3e738
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-11 20:59:05 +0530

    Refactor --> Added Other Important Points File

---
+++ JavaScript/_3_AddOn.js
@@ -0,0 +1,12 @@
+// Adding String and Integer --> It concatenated rather giving error like other languages
+console.log("sadiq"+6); 
+//Type of
+let a=false;
+console.log(typeof(a));
+//Dictionary Using Objects
+const dict={
+    d:"kjsvnihkstrndbov",
+    b:"jrgnihenrgo",
+    c:"ajrngijnewrg"
+}
+console.log(dict.b);
