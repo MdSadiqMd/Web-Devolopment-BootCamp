abadd2995e57f3dee7ec7f60cc30a11ff4482c59
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-11 20:37:29 +0530

    Refactor --> Added let and const

--- JavaScript/_1_Variables.js
+++ JavaScript/_1_Variables.js
@@ -1,4 +1,6 @@
 //In javascript we can change the type of variable in runtime
+//var is globally scoped
+//const and let are block scoped 
 function test() {
     var a = "3";
     var b = "8";
@@ -7,5 +9,11 @@ function test() {
     b = c;
     console.log("a is " + a);
     console.log("b is " + b);
+    let d="4";
+    d="6";
+    console.log(d);
+    const e="5";
+    //e="6"; // -->This throws an error as const can be Updated
+    console.log(e);
 }
 test();
