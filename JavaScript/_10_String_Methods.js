2c0c6e5c759333311736aa9c4e2dc48e4d9dd225
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-12 20:26:28 +0530

    Refactor --> Added Methods in Strings and Termenary Operator on Strings

---
+++ JavaScript/_10_String_Methods.js
@@ -0,0 +1,15 @@
+let name = "sadiq";
+let name1= "sadiq";
+console.log(name.length);       
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,4));
+console.log(name.slice(2));
+console.log(name.replace("ad","da"));
+console.log(name.concat(name1)); // concat not concate
+
+// Termenary Operators in Strings
+let a = "sadiq is a Boy";
+let b = "Boy";
+console.log(`${a.includes(b) ? 'Yes' : 'No'}`);
+
