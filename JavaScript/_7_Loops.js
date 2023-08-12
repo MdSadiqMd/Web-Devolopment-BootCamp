a50911bae0d681456ac54550ac7d61d5a47e4b22
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-12 19:43:37 +0530

    Refactor --> Added Loops in Javascript

---
+++ JavaScript/_7_Loops.js
@@ -0,0 +1,34 @@
+// for Loop
+for(let i=0;i<100;i++){ // ***If we used var here then it will be globally scoped (Remember for tricky Questions)
+    console.log(i);
+}
+
+// for-in Loops
+let obj={
+    "Harry":true,
+    "sadiq":false,
+    "Akhil":67,
+    "Ravi":undefined
+}
+for(let a in obj){
+    console.log(a+" "+obj[a]);
+}
+
+// for-of loops
+for(let b of "sadiq"){ // Here the Object must be iterable
+    console.log(b);
+}
+
+// While Loop
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+// Do-while loop
+let j=0;
+do{
+    console.log(j);
+    j++;
+}while(i=10);
