bf81054efd622917c74b9a0b26a11f78320c4888
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-12 20:09:37 +0530

    Refactor --> Added Strings and Template Literals in javascript

---
+++ JavaScript/_9_Strings.js
@@ -0,0 +1,11 @@
+// Strings can be defined in Single or Double quotations
+let a="sadiq";
+let b='sadiq1';
+console.log(a);
+console.log(b);
+
+// Template Literals 
+let c="a and b"; // Without template literals
+let d=`${a} and ${b}`; // ***Note : Template Literals are enclosed in tick brackets ` `cl
+console.log(c);
+console.log(d);
