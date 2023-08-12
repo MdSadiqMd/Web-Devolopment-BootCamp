b9d774c24e52e809183f36912e9b774c7e0a5a7a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-12 19:27:52 +0530

    Refactor --> Added Switch Case Statements

---
+++ JavaScript/_6_Switch_Case_Statements.js
@@ -0,0 +1,17 @@
+const readline=require('readline-sync')
+let age=readline.question("Enter your age : ");
+age=parseInt(age); //It's always better to convert it into desired Format 
+switch(age){
+    case 12:
+        console.log("dngvjneror");
+        break
+    case 13:
+        console.log("ddaguoeounb");
+        break
+    case 14:
+        console.log("eivjpierevnv");
+        break
+    default:
+        console.log("mvojnsrioedb");
+        break
+}
