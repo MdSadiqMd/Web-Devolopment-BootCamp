243119a5987d39c15eef76354504873a7c6cb694
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-17 20:04:27 +0530

    Docs --> Added theory of temporial Dead Zone and Hoisting of let and const

--- JavaScript/_12_Hoisting.js
+++ JavaScript/_12_Hoisting.js
@@ -18,6 +18,9 @@ function getName(){
     console.log("Hello World");
 }*/
 
+// Temporial Dead Zone Means the Time Between the let variable Created and the Value allocation to let Variable
+// Interview Question: Do let and const are Hoisted or not ? --> Expalin about temporial dead zone and ways to avoid it
+// To avoid Temporial Dead Zones Keep your Declaration on the Top Probably Known as "Shrinking the Tempoial Dead Zone to Zero"
 
 // Incase of Arrow Function
 getName(); // output : Error --> as here the getName function acts as variable and you're calling it as a function 
