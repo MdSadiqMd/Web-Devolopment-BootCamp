7caa12fed242fc359e8280767d9b2bc87a3db35d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-19 21:05:17 +0530

    Refactor --> Added Nested Functions

--- JavaScript/_8_Functions.js
+++ JavaScript/_8_Functions.js
@@ -12,6 +12,19 @@ function b(){
     console.log(x);
 }
 
+// Nested Function
+function f1() {
+    console.log("Hello");
+    function f2() {
+        console.log("World");
+    }
+    return f2;
+}
+
+f1();    // This will log "Hello" only
+f1()();  // (or) f2(); // This will log "Hello" followed by "World"
+
+
 // Arrow Function --> Prefer this over upper one
 const func=()=>{
     console.log("Sadiq");
