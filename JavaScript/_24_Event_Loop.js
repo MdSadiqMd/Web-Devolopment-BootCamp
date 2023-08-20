81ff9dbb41cabf9c93d88f545e470cfec55556ed
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-20 13:21:25 +0530

    Refactor --> Added Some Misconceptions of Event loops

--- JavaScript/_24_Event_Loop.js
+++ JavaScript/_24_Event_Loop.js
@@ -14,6 +14,12 @@
 // Starvation in Call Back --> Until and Unless all the functions in Microtask Queue get's Executed the callback Queue Functions will not Get Executed
 //                         --> Suppose the Microtask Queue Will call an another Microtask then it will increase the time of execution of the call bak Queue as that Newly created Microtask Queue Should be Executed this is called call back Starvation
 
+/*
+1. When does the event loop actually start? - Event loop, as the name suggests, is a single-thread, loop that is `almost infinite`. It's always running and doing its job. 
+2.  Are only asynchronous web API callbacks are registered in the web API environment? - YES, the synchronous callback functions like what we pass inside map, filter, and reduce aren't registered in the Web API environment. It's just those async callback functions that go through all this.
+3. Does the web API environment stores only the callback function and pushes the same callback to queue/microtask queue? - Yes, the callback functions are stored, and a reference is scheduled in the queues. Moreover, in the case of event listeners(for example click handlers), the original callbacks stay in the web API environment forever, that's why it's advised to explicitly remove the listeners when not in use so that the garbage collector does its job.
+4. How does it matter if we delay for setTimeout would be 0ms. Then callback will move to queue without any wait? No, there are trust issues with setTimeout(). The callback function needs to wait until the Call Stack is empty. So the 0 ms callback might have to wait for 100ms also if the stack is busy.
+*/ 
 
 // ***** Working of Browser tools in JS 
 // Example -1 :SetTimer() 
