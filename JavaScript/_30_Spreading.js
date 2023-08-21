aca89eed9ed359b9ef7265cf5195ef13e73d34b3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-08-21 19:00:37 +0530

    Refactor --> Added Spreading in JS

---
+++ JavaScript/_30_Spreading.js
@@ -0,0 +1,14 @@
+// We can see that the rray copied in b and if an element is removed from b then it also reflect in a
+// But we should not alter our original data
+var a=[1,2,3,4,5];
+var b=a;
+b.pop();
+console.log(a); // [ 1, 2, 3, 4 ]
+console.log(b); // [ 1, 2, 3, 4 ]
+
+// So we use Spreading which creates a copy of the array and doesnt alter original array
+var c=[1,2,3,4,5];
+var d=[...c];
+d.pop();
+console.log(c); // [ 1, 2, 3, 4, 5 ]
+console.log(d); // [ 1, 2, 3, 4 ]
