734c985e027f5428e12625038c260cc91e254f30
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-12 21:45:19 +0530

    Refactor --> Added GET,POST,PATCH and DELETE using Postman

--- NodeJS/_8_Building_HTTP_Server_REST.js
+++ NodeJS/_8_Building_HTTP_Server_REST.js
@@ -1,53 +1,94 @@
-const express=require("express");
-// Go to mackaroo.com and generate the JSON Data
-const users=require("./_8_MOCK_DATA.json");
-const app=express();
-const PORT=8000;
+// Open Post Man --> Click on "+" Sign (new Work Place Created)
+// for GET,PATCH and DELETE Method  --> select GET and write the url we write on browser (http://localhost:8000/api/users in case of GET) (***http not https) --> Press Send
+// for POST --> select POST --> select body --> select x-www-form-urlencoded --> write a demo key value pair needed to be appended --> Press Send
+const express = require("express");
+const users = require("./_8_MOCK_DATA.json");
+const fs = require("fs");
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // General HTML Rendering
-app.get("/users",(req,res)=>{
-    const html=`
+app.get("/users", (req, res) => {
+  const html = `
     <ul>
-        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
     </ul>
     `;
-    res.send(html);
+  res.send(html);
 });
 
 // RESTful API --> JSON Rendering
-app.get("/api/users",(req,res)=>{
-    return res.json(users);
+app.get("/api/users", (req, res) => {
+  return res.json(users);
 });
 
-// *****Browsers will default intake Only GET requests 
-// *** Dynamic URL rendering --> If we write :x in an url in code then that x is dynamic
-//                           --> a Function which returns the person data as per given id value
-/*app.get("/api/users/:id",(req,res)=>{
-    const id=Number(req.params.id);
-    const user=users.find((user)=>user.id===id);
+// Dynamic URL rendering
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  if (user) {
     return res.json(user);
+  } else {
+    return res.status(404).json({ error: "User not found" });
+  }
 });
 
-// Post
-app.post("/api/users",(req,res)=>{
-    // Create new User
-    return res.json({status:"pending"});
+// Create a new user
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  const newUserId = users.length + 1;
+  const newUser = { ...body, id: newUserId };
+  
+  users.push(newUser);
+
+  fs.writeFile("./_8_MOCK_DATA.json", JSON.stringify(users), (err) => {
+    if (err) {
+      return res.status(500).json({ error: "Error saving user data" });
+    }
+    return res.status(201).json({ status: "success", id: newUserId });
+  });
 });
 
-// Patch
-app.patch("/api/users/:id",(req,res)=>{
-    // Edit the user with id
-    return res.json({status:"pending"});
+// Update user by ID (you can modify this part as needed)
+app.patch("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedUserData = req.body;
+  const userIndex = users.findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    users[userIndex] = { ...users[userIndex], ...updatedUserData };
+    fs.writeFile("./_8_MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Error saving user data" });
+      }
+      return res.json({ status: "success" });
+    });
+  } else {
+    return res.status(404).json({ error: "User not found" });
+  }
 });
 
-// Delete
-app.delete("/api/users/:id",(req,res)=>{
-    // Deleting the User with id
-    return res.json({status:"pending"});
-});*/
+// Delete user by ID (you can modify this part as needed)
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const userIndex = users.findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    const deletedUser = users.splice(userIndex, 1)[0];
+    fs.writeFile("./_8_MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res.status(500).json({ error: "Error saving user data" });
+      }
+      return res.json({ status: "success", user: deletedUser });
+    });
+  } else {
+    return res.status(404).json({ error: "User not found" });
+  }
+});
 
 // ***(or)
-app
+/*app
     .route("/api/users/:id") // By Using this route we can peform all operations on one url rather than creating different HTTP methods on same url
     .get("/api/users/:id",(req,res)=>{
         const id=Number(req.params.id);
@@ -65,7 +106,7 @@ app
     .delete("/api/users/:id",(req,res)=>{
         // Deleting the User with id
         return res.json({status:"pending"});
-    });
+    });*/
 
 
 app.listen(PORT,()=>console.log(`Server Started at ${PORT}`));
