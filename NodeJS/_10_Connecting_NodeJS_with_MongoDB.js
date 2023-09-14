55f37d90c24118f44488b2d33b1731cfc14514c2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-14 19:54:16 +0530

    Refactor --> Added Connecting NodeJS with MongoDB

--- NodeJS/_10_Connecting_NodeJS_with_MongoDB.js
+++ NodeJS/_10_Connecting_NodeJS_with_MongoDB.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 8000;
 
-// Connect to MongoDB (make sure to replace the connection string)
-mongoose.connect('mongodb://127.0.0.1:27017/FirstMongodB', {
+// Connecting to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/MongoDataBase", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+  .catch(err => console.log("MongoDB error", err));
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
@@ -25,62 +25,67 @@ const userSchema = new mongoose.Schema({
   },
   jobTitle: String,
   gender: String,
-},{timestamps:true}); //It will the user created Time
+}, { timestamps: true }); // It will store the user creation time
 
-// Create a user model
 const User = mongoose.model("User", userSchema);
 
-// Middleware for JSON request parsing
-app.use(express.json());
+app.use(express.json()); // Parse JSON requests
 
-// Middleware for logging
 app.use((req, res, next) => {
   console.log("Hi from Middleware");
   console.log(`${Date.now()} : ${req.ip} : ${req.method} : ${req.path}`);
-  next();
+  next(); // If we don't call next(), the remaining code will not run
 });
 
-// API Routes
-
-// Get all users
-app.get("/api/users", async (req, res) => {
-  const allDbUsers=await User.find({}); // Empty parenthesis means return all users
-  try {
-    const html=`
+// General HTML Rendering
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  const html = `
     <ul>
-      ${allDbUsers.map((user)=> `<li>${user.firstName} - ${user.email}</li>`).join("")}
+      ${users.map(user => `<li>${user.firstName}</li>`).join("")}
     </ul>
-    `
-    res.send(html);
-  } catch (err) {
-    console.error("Error fetching users:", err);
-    return res.status(500).json({ error: "Internal Server Error" });
+  `;
+  res.send(html);
+});
+
+// RESTful API --> JSON Rendering
+app.get("/api/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// Dynamic URL rendering
+app.get("/api/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+  if (user) {
+    return res.json(user);
+  } else {
+    return res.status(404).json({ error: "User not found" });
   }
 });
 
 // Create a new user
 app.post("/api/users", async (req, res) => {
-  try {
-    const { firstName, lastName, email, jobTitle, gender } = req.body;
-
-    const newUser = new User({
-      firstName,
-      lastName,
-      email,
-      jobTitle,
-      gender,
-    });
-
-    await newUser.save();
-
-    // Log the user data in the terminal
-    console.log("User created:", newUser);
-
-    return res.status(201).json({ msg: "User created successfully" });
-  } catch (err) {
-    console.error("Error creating user:", err);
-    return res.status(500).json({ error: "Internal Server Error" });
+  const body = req.body;
+  if (
+    !body ||
+    !body.firstName ||
+    !body.lastName ||
+    !body.email ||
+    !body.jobTitle
+  ) {
+    return res.status(400).json({ msg: "All fields are required" });
   }
+
+  const result = await User.create({
+    firstName: body.firstName,
+    lastName: body.lastName,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.jobTitle,
+  });
+  return res.status(201).json({ msg: "success", user: result }); // Return the created user object
 });
 
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
