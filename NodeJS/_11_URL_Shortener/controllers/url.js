22b6ec9547656ec1f98ab3eaa8edb4b68469c450
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-14 22:22:51 +0530

    Refactor --> Added ShortID Modules

--- NodeJS/_11_URL_Shortener/controllers/url.js
+++ NodeJS/_11_URL_Shortener/controllers/url.js
@@ -1,3 +1,16 @@
+const {nonoid, nanoid}=require("nanoid");
+const url=require("../models/url")
 async function handleGenerateNewShortURL(req,res){
-    
+    const body=req.body;
+    if(!body.url){
+        return res.status(400).json({error:"url is required"})
+    }
+    const shortId=nanoid(8);
+    await URL.create({
+        shortId:shortId,
+        redirectURL:body.url,
+        visitHistory:[],
+    });
+    return res.json({id:shortId});
 }
+module.exports=handleGenerateNewShortURL;
--- NodeJS/_11_URL_Shortener/index.js
+++ NodeJS/_11_URL_Shortener/index.js
@@ -1,7 +1,14 @@
 const express=require("express");
+const {connectToMongoDB}=require("../_11_URL_Shortener/connection")
+const urlRoute=require("./routes/url")
 const app=express();
 const PORT=8001;
 
+connectToMongoDB("mongodb://localhost:27017/short-url").then(()=>{
+    console.log("MongoDB Connected");
+})
+
+app.use("/url",urlRoute);
 
 app.listen(PORT,()=>{
     console.log(`Server Started at :${PORT}`);
