b37bf87421437f958e09f55d13573d2da10def79
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-16 21:54:58 +0530

    Chore--> Added Neccesary Modules

--- NodeJS/_13_EJS_TODO_List/app.js
+++ NodeJS/_13_EJS_TODO_List/app.js
@@ -10,8 +10,7 @@ app.get("/",(req,res)=>{
     let options={weekday:'long',year:'numeric',month:'long',day:'numeric'};
     let today=new Date();
     let day=today.toLocaleDateString("en-US",options);
-    res.send(day);
-    
+    res.render("list",{KindOfDay:day}); // It finds list Folder in views(default) folder
 });
 
 app.listen(8000,()=>{console.log("Server Started at 8000")})
