f8327c8fa5c4cd856e28da42127705eff67ac5d7
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-16 21:39:29 +0530

    Refactor --> Added Boiler plate Code

--- NodeJS/_13_TODO_List/app.js
+++ NodeJS/_13_TODO_List/app.js
@@ -1,1 +1,13 @@
 const express=require("express");
+const bosyParser=require("body-parser");
+
+const app=express();
+app.use(bosyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+    let today=new Date().getDate();
+    console.log(today);
+});
+
+app.listen(8000,()=>{console.log("Server Started at 8000")})
