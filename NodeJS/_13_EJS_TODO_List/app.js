cf59d27b46bd37dd290852b4e484fc7c47b06640
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-16 22:48:42 +0530

    Refactor --> Added List.ejs

--- NodeJS/_13_EJS_TODO_List/app.js
+++ NodeJS/_13_EJS_TODO_List/app.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let today = new Date();
     let day = today.toLocaleDateString("en-US", options);
-    res.render("list", { KindOfDay: day }); // It finds the "list" file in the "views" folder
+    res.render('list', { KindOfDay: day }); // It finds the "list" file in the "views" folder
 });
 
 app.listen(8000, () => {
