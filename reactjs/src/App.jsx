c49f99b43ed1956b8e25dd460ccf8684f91ca776
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-19 18:53:49 +0530

    Refactor --> Added Hooks of UseState in React

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,24 +1,38 @@
-// app.jsx
-import React from "react";
+// This useState Particular hook is used to Change the value of a variable in N different places in a website
+import React, { useState } from "react"; // Here, we are importing useState hook
 
 function App() {
-  let counter = 5;
+  const [counter, setCounter] = useState(5); // Here, we use the useState hook which returns two values in the array form one the variable and the other the variable updation
 
   const handleAddValue = () => {
-    counter=counter+1;
+    if(counter<=20){
+        setCounter(counter + 1); 
+        //(or)
+        //counter=counter+1;
+        //setCounter(counter);
+    } else {
+        alert("Exceeded the counter Limit");
+    }
   };
 
   const handleRemoveValue = () => {
-    counter=counter-1;
+    if(counter>=0){
+        setCounter(counter - 1); 
+        //(or)
+        //counter=counter-1;
+        //setCounter(counter);
+    } else {
+        alert("Exceeded the counter Limit");
+    }
   };
 
   return (
     <div>
       <h1>Hello World</h1>
       <h2>Counter Value : {counter}</h2>
-      <button onClick={handleAddValue}>Add Value</button>
+      <button onClick={handleAddValue}>Add Value {counter} </button>
       <br />
-      <button onClick={handleRemoveValue}>Remove Value</button>
+      <button onClick={handleRemoveValue}>Remove Value {counter} </button>
     </div>
   );
 }
