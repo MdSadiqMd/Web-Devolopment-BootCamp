0a7f4256e5f18a0bcba36640c85d3d1049bbfa6a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-20 21:29:58 +0530

    Docs --> Added Documentation of Code

--- reactjs/src/7_App.jsx
+++ reactjs/src/7_App.jsx
@@ -1,17 +1,17 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false);
+  // Here we have the variables which we give default values using useState
+  const [length, setLength] = useState(8); // Default length of password
+  const [numberAllowed, setNumberAllowed] = useState(false); // as check box we give default unchecked check box means false
   const [charAllowed, setCharAllowed] = useState(false)
-  const [password, setPassword] = useState("")
+  const [password, setPassword] = useState(""); // First we have empty password and fastly as the page renders we ceate one
 
-  //useRef hook
-  const passwordRef = useRef(null)
+  const passwordRef = useRef(null); // The useRef hook is used to take reference of any object in the web and manipulate it --> we use this to take refernce of generated password and when the password is copied the blue texture of password that it has copied
 
-  const passwordGenerator = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const passwordGenerator = useCallback(() => {  // useCallback takes two inputs function,dependencies that manipulated by function
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     if (numberAllowed) str += "0123456789"
     if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
@@ -20,19 +20,16 @@ function App() {
       pass += str.charAt(char)
       
     }
+    setPassword(pass) // This line is to update the existing password when a parameter is changed
+  }, [length, numberAllowed, charAllowed, setPassword]); // ** Here the last dependecy parameter we have given setpassword other than password as it is a way for optimization that when we add a parameter like add numbers rather than ceating a whole new password it just adds numbers in existing password
 
-    setPassword(pass)
-
-
-  }, [length, numberAllowed, charAllowed, setPassword])
-
-  const copyPasswordToClipboard = useCallback(() => {
-    passwordRef.current?.select();
-    passwordRef.current?.setSelectionRange(0, 999);
+  const copyPasswordToClipboard = useCallback(() => { // This function is behind the copying of code 
+    passwordRef.current?.select(); 
+    passwordRef.current?.setSelectionRange(0, 999); // The selection range is the length of the password that to be copied for more understanding setSelectionRange(0, 3) and copy a password
     window.navigator.clipboard.writeText(password)
   }, [password])
 
-  useEffect(() => {
+  useEffect(() => { // use effect takes two values funtion,dependencies but the difference between useCallback and useEffect is useEffect will run the function only any one of the dependencies get change and useCallback is used to run the function based on dependencies
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
@@ -46,7 +43,7 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
-            ref={passwordRef}
+            ref={passwordRef} // we need to pass a ref Object in the text that need to be manipulated by reference
         />
         <button
         onClick={copyPasswordToClipboard}
@@ -62,7 +59,7 @@ function App() {
         max={100}
         value={length}
          className='cursor-pointer'
-         onChange={(e) => {setLength(e.target.value)}}
+         onChange={(e) => {setLength(e.target.value)}} // Manipulating the length using ***variable setLength as given in top
           />
           <label>Length: {length}</label>
       </div>
