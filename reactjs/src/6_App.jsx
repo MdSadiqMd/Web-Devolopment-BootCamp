c3b91f4441ca98f30309b2b33e714f945e084010
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-20 18:59:45 +0530

    Refactor --> Added Background changer using React

---
+++ reactjs/src/6_App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+            <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3">
+                <button onClick={()=>setColor("red")} className="outline-none px-4 py-1 rounded text-white shadow-lg" style={{backgroundColor:"red"}}>Red</button>
+                <button onClick={()=>setColor("green")} className="outline-none px-4 py-1 rounded text-white shadow-lg" style={{backgroundColor:"green"}}>Green</button>
+                <button onClick={()=>setColor("blue")} className="outline-none px-4 py-1 rounded text-white shadow-lg" style={{backgroundColor:"blue"}}>Blue</button>
+                <button onClick={()=>setColor("black")} className="outline-none px-4 py-1 rounded text-white shadow-lg" style={{backgroundColor:"Black"}}>Black</button>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
