40f127bcc4bda15ef9a3bd9a3fe036e274cb1036
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-23 19:08:14 +0530

    Refactor --> Added Custom hook

---
+++ reactjs/src/8_hooks/useCurrenctInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+// Here we create a Custom hook
+
+function useCurrencyInfo(){
+    const [data,setData]=useState({});
+    useEffect(()=>{
+       fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`) 
+       .then((res)=>res.json())
+       .then((res)=>setData(res[currency]))
+    },[currency]);
+    console.log(data);
+    return data;
+}
+
+export default useCurrencyInfo;
