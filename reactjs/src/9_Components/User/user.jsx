6e5b54955ab17de9f96e654f6da1ea21c19c5ede
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-24 16:04:17 +0530

    Refactor --> Added User Component

---
+++ reactjs/src/9_Components/User/user.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function User(){
+    const {userid} =useParams()
+    return(
+        <div>
+            User: {userid}
+        </div>
+    )
+}
+
+export default User
--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Contact from "./9_Components/Contact Us/contact";
 
 // Creating a router
-const router=createBrowserRouter([
+/*const router=createBrowserRouter([
     {
         path: '/',
         element: <App />,
@@ -15,10 +16,25 @@ const router=createBrowserRouter([
             {
                 path:"about",
                 element:<About />
+            },
+            {
+                path:"/contact",
+                element:<Contact />
             }
         ]
     }
-])
+]); */
+
+// (or)
+const router =createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route path="" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+        </Route>
+    )
+)
 
 ReactDOM.render(
   <React.StrictMode>
