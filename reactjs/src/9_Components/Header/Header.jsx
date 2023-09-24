3efeef72df82879931983911f693fff8274fb2a1
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-24 15:29:40 +0530

    Refactor --> Added Header Component

--- reactjs/src/9_Components/Header/Header.jsx
+++ reactjs/src/9_Components/Header/Header.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import {Link,NavLink} from "react-router-dom"; // Interview Question : Why we use Link in place of href or a tag (HTML) because the a link/tag which refresh whole page everytime
+
 export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
@@ -31,8 +34,8 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                    className={() =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    className={({isActive}) => // The class name is written in this way because we need to know which page (or) "/" we are in so we write a callback function
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
