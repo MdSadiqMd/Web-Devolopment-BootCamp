bac0f8563311176219faef8612a3d62aeac3484d
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-25 20:32:52 +0530

    Chore --> Resolved Vulnerabilities

--- reactjs/src/10_App.jsx
+++ reactjs/src/10_App.jsx
@@ -1,72 +1,74 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { useTable } from "react-table";
 
+const data = [
+    {
+        id: 1,
+        gender: "Male",
+        salary: 10000,
+    },
+    {
+        id: 2,
+        gender: "Male",
+        salary: 20000,
+    },
+    {
+        id: 3,
+        gender: "Male",
+        salary: 30000,
+    },
+];
 
-const data=[
+const columns = [
     {
-        id:1,
-        gender:"Male",
-        salary:10000,
+        Header: "ID",
+        accessor: "id",
     },
     {
-        id:2,
-        gender:"Male",
-        salary:20000,
+        Header: "Gender",
+        accessor: "gender",
     },
     {
-        id:3,
-        gender:"Male",
-        salary:30000,
-    }
-]
+        Header: "Salary",
+        accessor: "salary",
+    },
+];
 
-const coloumns=[
-    { 
-        Header:"ID", // Header is which display in UI
-        accessor:"id", // Accessor is which we use to manipulate values in UI
-    },
-    {
-        Header:"Gender",
-        accessor:"gender",
-    },
-    {
-        Header:"Salary",
-        accessor:"salary",
-    },
-]
-
-function App(){
-    const {getTableProps,getTableBodyProps,headerGroups,rows,prepareRow}=useTable({
-        coloumns,
+function App() {
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+        columns,
         data,
     });
 
     return (
         <div className="container">
-            <table {...getTableProps()} >
+            <table {...getTableProps()}>
                 <thead>
-                    {headerGroups.map((hg)=>(
-                        <tr {hg.getHeaderProps()}></tr>
-                    ))}
-
-                    /*<tr>
-                        <th>Id</th>
-                        <th>Gender</th>
-                        <th>Salary</th>
-                    </tr>*/
-                </thead>
-                <tbody>
-                    {data.map((i)=>{
-                        <tr>
-                            <td>{i.id}</td>
-                            <td>{i.gender}</td>
-                            <td>{i.salary}</td>
+                    {headerGroups.map((headerGroup) => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column) => (
+                                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                            ))}
                         </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) => {
+                                    return (
+                                        <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                                    );
+                                })}
+                            </tr>
+                        );
                     })}
                 </tbody>
             </table>
         </div>
     );
-};
+}
 
-export default App
+export default App;
