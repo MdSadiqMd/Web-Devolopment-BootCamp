fce3d19ab287b834fcbce31baa90dcfff31bc65c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-25 20:11:39 +0530

    Refactor --> Added Table in react

--- reactjs/src/10_App.jsx
+++ reactjs/src/10_App.jsx
@@ -1,11 +1,46 @@
 import React from "react";
 
+const data=[
+    {
+        id:1,
+        gender:"Male",
+        salary:10000,
+    },
+    {
+        id:2,
+        gender:"Male",
+        salary:20000,
+    },
+    {
+        id:3,
+        gender:"Male",
+        salary:30000,
+    }
+]
+
 function App(){
     return (
         <div className="container">
-        
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Gender</th>
+                        <th>Salary</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((i)=>{
+                        <tr>
+                            <td>{i.id}</td>
+                            <td>{i.gender}</td>
+                            <td>{i.salary}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
         </div>
-    )
-}
+    );
+};
 
 export default App
