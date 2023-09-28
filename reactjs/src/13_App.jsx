d4b82de1ecadb14f7fd95868f00247748d46a0f4
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-28 19:24:45 +0530

    Refactor --> Added functionalities

--- reactjs/src/13_App.jsx
+++ reactjs/src/13_App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {TodoProvider} from './13_Context/TodoContext'
 
 function App() {
@@ -16,6 +16,17 @@ function App() {
     const toggleComplete=(id)=>{
         setTodos((prev)=>prev.map((prevTodo)=>prevTodo === id ? {...prevTodo,completed: !prevTodo.completed}:prevTodo))
     }
+
+    useEffect(()=>{
+        const todos=JSON.parse(localStorage.getItem("todos"))
+        if(todos && todos.length > 0){
+            setTodos(todos)
+        }
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem("todos",JSON.stringify(todos))
+    },[todos])
 
     return(
         <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
