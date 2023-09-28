5998f4fb36fb48bb7b97c67a8138e06b649339a6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-28 19:05:18 +0530

    Refactor --> Added index file for exports

--- reactjs/src/13_App.jsx
+++ reactjs/src/13_App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {TodoProvider} from './13_Context/TodoContext'
 
 function App() {
-  return (
-    <div>App</div>
-  )
+    const [todos,useTodos]=useState([])
+    return(
+        <>
+        <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+        </div>
+        </>
+    )
 }
 
 export default App
--- reactjs/src/13_Context/TodoContext.js
+++ reactjs/src/13_Context/TodoContext.js
@@ -1,22 +1,22 @@
-import { createContext,useContext } from "react";
+import {createContext, useContext} from "react"
 
-export const TodoContext=createContext({
-    todos:[
+export const TodoContext = createContext({
+    todos: [
         {
-            id:1,
-            todo:"Todo msg",
-            completed:false,
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
         }
     ],
-    addTodo:(todo)=>{},
-    updateTodo:(id,tool)=>{},
-    deleteTodo:(id)=>{},
-    toggleComplete:(id)=>{}
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 })
 
-export const useTodo=()=>{
+
+export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-export const TodoProvider=TodoContext
-export {TodoContext,TodoProvider,useTodo}
+export const TodoProvider = TodoContext.Provider
