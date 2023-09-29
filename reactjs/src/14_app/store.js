4a470173f1ad0f36a2717b4f6c8cfe52a639aab8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-29 20:46:39 +0530

    Refactor --> Added Redux in project

--- reactjs/src/14_app/store.js
+++ reactjs/src/14_app/store.js
@@ -1,4 +1,7 @@
 // For react redux
 import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../14_features/todo/todoSlice';
 
-export const store=configureStore({})
+export const store=configureStore({
+    reducer: todoReducer
+})
