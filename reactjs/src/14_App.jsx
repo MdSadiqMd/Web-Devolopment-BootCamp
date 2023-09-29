b9f7a37012586911d23dd1a46e5a213ef4e8513a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-09-29 20:20:42 +0530

    Refactor --> Added Redux config

--- reactjs/src/14_App.jsx
+++ reactjs/src/14_App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { ReactDOM } from 'react-dom'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    
+    </>
   )
 }
 
--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./14_App";
+import { Provider } from "react-redux";
+import { store } from './14_app/store';
 
-ReactDOM.render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
       <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </Provider>
 );
