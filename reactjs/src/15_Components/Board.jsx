d2f78465204c45d68e1877cf896ac0a61c2e094f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-02 19:43:05 +0530

    Refactor --> Added the winner logic

--- reactjs/src/15_Components/Board.jsx
+++ reactjs/src/15_Components/Board.jsx
@@ -1,17 +1,50 @@
 import React, { useState } from 'react';
-import Square from './squares'
+import Square from './Square';
 
 function Board() {
   const [state, setState] = useState(Array(9).fill(null));
   const [isX, setX] = useState(true);
 
-  const checkWinner
+  const winnerLogic = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkWinner = () => {
+    for (const logic of winnerLogic) {
+      const [a, b, c] = logic;
+      if (state[a] && state[a] === state[b] && state[a] === state[c]) {
+        return state[a];
+      }
+    }
+    return null;
+  };
 
   const handleClick = (index) => {
     const copyState = [...state];
-    copyState[index] = isX ? "X" : "O";
+    if (copyState[index] || checkWinner()) {
+      return;
+    }
+    copyState[index] = isX ? 'X' : 'O';
     setState(copyState);
     setX(!isX);
+  };
+
+  const winner = checkWinner();
+  let status;
+
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else if (state.every((square) => square)) {
+    status = 'It\'s a draw!';
+  } else {
+    status = 'Next player: ' + (isX ? 'X' : 'O');
   }
 
   return (
