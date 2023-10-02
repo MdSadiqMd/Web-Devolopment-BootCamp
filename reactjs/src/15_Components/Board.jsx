7d0da5cd58690b3892f3997a94193dc3e1f964ae
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-02 18:43:10 +0530

    Reafactor --> Added Components

---
+++ reactjs/src/15_Components/Board.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import square from './squares'
+
+function Board() {
+  return (
+    <div className='board-container'>
+        <div className='board-row'>
+            <square />
+            <square />
+            <square />
+        </div>
+        <div className='board-row'>
+            <square />
+            <square />
+            <square />
+        </div>
+        <div className='board-row'>
+            <square />
+            <square />
+            <square />
+        </div>
+    </div>
+  )
+}
+
+export default Board
---
+++ reactjs/src/15_Components/squares.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const square=()=>{
+    return(
+        <div 
+        style={{}}
+        className="square">
+            <h5>null</h5>
+        </div>
+    );
+};
+
+export default square;
