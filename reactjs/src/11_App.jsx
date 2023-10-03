c2062f71652dda0455971688c495734caa7e2f24
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-03 19:17:01 +0530

    Docs --> Added constext Provider doc

--- reactjs/src/11_App.jsx
+++ reactjs/src/11_App.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import Counter from "./11_Components/Counter"
 import { CounterContext } from "./11_Context/Counter";
 
-// Context API --> Rather than passing a value or a variable through props we create a folder of context and keep values in it (as the name suggests Context API) and use it from it rather falling in prop hell
+// Context API --> Rather than passing a value or a variable through props we create a folder of context and keep values in it (as the name suggests Context API) and use it from it rather falling in prop drilling and These whole Context API and Components are wrapped into Context provider and access the whole at once
 function App(){
     const counterState=useContext(CounterContext)
     return(
