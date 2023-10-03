ef2d1d2b066ab4455f6bcfdf0634846f1d61dad8
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-03 18:59:56 +0530

    Docs --> typo error fixed

--- JavaScript/_30_Spreading.js
+++ JavaScript/_30_Spreading.js
@@ -1,4 +1,4 @@
-// We can see that the rray copied in b and if an element is removed from b then it also reflect in a
+// We can see that the array copied in b and if an element is removed from b then it also reflect in a
 // But we should not alter our original data
 var a=[1,2,3,4,5];
 var b=a;
@@ -6,7 +6,7 @@ b.pop();
 console.log(a); // [ 1, 2, 3, 4 ]
 console.log(b); // [ 1, 2, 3, 4 ]
 
-// So we use Spreading which creates a copy of the array and doesnt alter original array
+// So we use Spreading which creates a copy of the array and doesn't alter original array
 var c=[1,2,3,4,5];
 var d=[...c];
 d.pop();
