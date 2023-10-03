ef9da58908ce61f8cf46c80ec64a3690192bb2f9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-03 20:48:13 +0530

    Refactor --> Added Edit text to memes

---
+++ reactjs/src/17_Components/Text.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Text=()=>{
+    return(
+        <h1>Iam a text</h1>
+    )
+}
+
+export default Text;
--- reactjs/src/17_Pages/Edit.jsx
+++ reactjs/src/17_Pages/Edit.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import Text from "../17_Components/Text";
 
 const EditPage=()=>{
     const [params]=useSearchParams();
+    const [count,setCount]=useState(0);
+
+    const addText=()=>{
+        setCount(count+1);
+    }
     return (
         <div>
-            <img src={params.get("url")} width="250px" alt="meme"/>
+            <div>
+                <img src={params.get("url")} width="250px" alt="meme"/>
+                {
+                    Array(count).fill(0).map((e)=>{
+                        <Text />
+                    })
+                }
+            </div>
+            <button onClick={addText}>Add Text</button>
         </div>
     )
 }
