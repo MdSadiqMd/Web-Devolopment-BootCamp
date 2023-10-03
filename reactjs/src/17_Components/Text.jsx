385627a565b7234769f96f297aac67588012ffb6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-03 20:59:59 +0530

    Refactor -->A dded Edit the text of the Image

--- reactjs/src/17_Components/Text.jsx
+++ reactjs/src/17_Components/Text.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable"
 
 const Text=()=>{
+    const [editMode,setEditMode]=useState(false);
+    const [val,setVal]=useState('Double click to Edit');
+
     return(
-        <h1>Iam a text</h1>
+        <Draggable>
+            {
+                editMode ? (<input onDoubleClick={e=>setEditMode(false)} value={val} onChange={(e)=>setVal(e.target.value)} /> 
+                ):(
+                <h1 onDoubleClick={e=>setEditMode(true)}>{val}</h1>)
+            }
+        </Draggable>
     )
 }
 
--- reactjs/src/17_Pages/Edit.jsx
+++ reactjs/src/17_Pages/Edit.jsx
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Text from "../17_Components/Text";
 
-const EditPage=()=>{
-    const [params]=useSearchParams();
-    const [count,setCount]=useState(0);
+const EditPage = () => {
+    const [params] = useSearchParams();
+    const [count, setCount] = useState(0);
 
-    const addText=()=>{
-        setCount(count+1);
+    const addText = () => {
+        setCount(count + 1);
     }
+
     return (
         <div>
             <div>
-                <img src={params.get("url")} width="250px" alt="meme"/>
+                <img src={params.get("url")} width="250px" alt="meme" />
                 {
-                    Array(count).fill(0).map((e)=>{
-                        <Text />
-                    })
+                    Array(count).fill(0).map((e, index) => (
+                        <Text key={index} />
+                    ))
                 }
             </div>
             <button onClick={addText}>Add Text</button>
