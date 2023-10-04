e658b2f668db2aa34e18eac389ab138d95464092
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-04 21:46:10 +0530

    Docs --> Added redux Documentation

--- reactjs/src/14_index_Redux.js
+++ reactjs/src/14_index_Redux.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./14_App";
 import { Provider } from "react-redux";
 import { store } from './14_app/store';
-
+// React redux is used to counter prop drilling solving the same problem that Context API solves 
+// In react redux where there UI doesn't directly update/manipulate the props it will send a event of manipulation to redux store and redux store updates using the reducer
+// Context API is used when we have to handle the prop drilling and react redux conquer the updation of props 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <App />
