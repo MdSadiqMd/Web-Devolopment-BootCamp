b3f364bd58c20d6f0f768c9564694490c8468fb2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-04 22:24:41 +0530

    Docs --> Added redux API procedure

--- reactjs/src/18_index_Redux_API_Fetching.js
+++ reactjs/src/18_index_Redux_API_Fetching.js
@@ -4,6 +4,7 @@ import App from "./18_App";
 import { Provider } from "react-redux";
 import { store } from "./18_redux/store";
 // For API Calling using Redux we use createAsyncThunk (this handle Async data)
+// For output click on fetch button and see the redux extension
 ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
