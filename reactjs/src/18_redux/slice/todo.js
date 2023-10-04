83fab8586ade34a22bfed03c8fd71465986a663a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-04 21:55:41 +0530

    Refactor --> Added store config

---
+++ reactjs/src/18_redux/slice/todo.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+import todoSlice from "../../14_features/todo/todoSlice";
+
+const todoslice =createSlice({
+    reducer:{
+
+    }
+})
+
+export default todoSlice.reducer
--- reactjs/src/18_redux/store.js
+++ reactjs/src/18_redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import todoSlice from "../14_features/todo/todoSlice";
 
 export const store =configureStore({
     reducer:{
-        
+        todo:todoSlice
     }
 })
