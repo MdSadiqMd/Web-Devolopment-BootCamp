82f8ccc1e76f2bd6c0e6dad56e7e0840535feab2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-08 20:28:23 +0530

    Refactor --> added firebase db controlling

--- reactjs/src/19_App.jsx
+++ reactjs/src/19_App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { getDatabase , ref , set } from "firebase/database"
+import { app } from './19_firebase'
+
+const db=getDatabase(app);
 
 function App() {
+
+  const putdata=()=>{
+    set(ref(db,"users/sadiq"),{
+      id:1,
+      name:"sadiq",
+      age:21,
+    })
+  }
+
   return (
-    <div>App</div>
+    <div className='App'>
+      <h1>Firebase</h1>
+      <button onClick={putdata}>putdata</button>
+    </div>
   )
 }
 
---
+++ reactjs/src/19_firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAGBSTMiNYZbSqTg_9W61cZtOYKVlF7WW0",
+    authDomain: "fir-17e4a.firebaseapp.com",
+    projectId: "fir-17e4a",
+    storageBucket: "fir-17e4a.appspot.com",
+    messagingSenderId: "333825579797",
+    appId: "1:333825579797:web:dbe5ad899eb7205b56e71c",
+    databaseURL: "https://fir-17e4a-default-rtdb.firebaseio.com",
+};
+
+// This connects our web app with firebase
+export const app = initializeApp(firebaseConfig);
