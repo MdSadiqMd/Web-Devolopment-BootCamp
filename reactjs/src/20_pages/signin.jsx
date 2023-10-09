df3ccfa80097abc80f1d21fc2ced7ce329df716e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-09 19:40:22 +0530

    Refactor --> Added Signin Authorizationusing Firebase

--- reactjs/src/20_pages/signin.jsx
+++ reactjs/src/20_pages/signin.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { app } from '../20_firebase';
 
 function Signin() {
+
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+
+  const signinUser=()=>{
+    signInWithEmailAndPassword(auth,email,password).then((value)=>alert("signin Success"))
+    .catch((err)=>alert(err))
+  }
+  
   return (
     <div className="signinPage">
     <h1>Sign In</h1>
     <h1>Signin Page</h1>
     <label htmlFor="email">Email</label>
-    <input type="email" required placeholder="Enter your email" />
+    <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" required placeholder="Enter your email" />
     <label htmlFor="password">Password</label>
-    <input type="password" required placeholder="Enter your Password" />
+    <input  onChange={(e)=>setPassword(e.target.value)} value={password} type="password" required placeholder="Enter your Password" />
     <button onClick={createUser}>Sign In</button>
     </div>
   )
