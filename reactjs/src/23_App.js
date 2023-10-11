476912def2b8af65e14b99eed1279d37145978f3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-11 20:08:07 +0530

    Refactor --> Added Register Page

--- reactjs/src/23_App.js
+++ reactjs/src/23_App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes,Route } from 'react-router-dom';
+import RegisterPage from './23_pages/Register';
 
 function App() {
   return (
     <Routes>
         <Route path='/' element={<h1>Home</h1>} />
         <Route path='/login' element={<h1>Login</h1>} />
+        <Route path='/register' element={<RegisterPage />} />
     </Routes>
   )
 }
