a25e622583bac58752d5f188207640fdc19a91ff
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-13 20:35:49 +0530

    Refactor --> Added NavBar

--- reactjs/src/23_App.js
+++ reactjs/src/23_App.js
@@ -3,14 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes,Route } from 'react-router-dom';
 import RegisterPage from './23_pages/Register';
 import LoginPage from './23_pages/Login';
+import myNavbar from './23_Components/Navbar';
 
 function App() {
   return (
-    <Routes>
+    <div>
+      <myNavbar />
+      <Routes>
         <Route path='/' element={<h1>Home</h1>} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
-    </Routes>
+      </Routes>
+    </div>
   )
 }
 
