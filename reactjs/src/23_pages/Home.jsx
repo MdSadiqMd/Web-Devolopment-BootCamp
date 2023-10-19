63cbf0f2e86b58be252562d09c6847d1558b7091
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-19 21:05:38 +0530

    Refactor --> Added Home Page Listing

--- reactjs/src/23_pages/Home.jsx
+++ reactjs/src/23_pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useFirebase } from '../23_Context/Firebase'
 
 function HomePage() {
+    const firebase=useFirebase();
+
+    const [books, setBooks] = useState([])
+
+    useEffect(()=>{
+        firebase.listAllBooks().then((books)=>setBooks(books.docs))
+    },[])
   return (
-    <div className="container">List Books Here</div>
+    <div className="container">
+        {(books.map(book => <li>{}</li>))}
+    </div>
   )
 }
 
