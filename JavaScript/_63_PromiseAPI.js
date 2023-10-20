bba02d77a8886bab11e6fe478f54224f01fb262b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-20 21:41:51 +0530

    Docs --> Added the Promise API Docs

--- JavaScript/_63_PromiseAPI.js
+++ JavaScript/_63_PromiseAPI.js
@@ -1,4 +1,7 @@
 /*
+
+The Example of the Promise API where the Fecthing of Data is Done from three Random API's P1 in 3sec,P2 in 1 sec and P3 in 2sec
+
                                     If all are Successful               If Any one Fails
 
 Promise.all([p1, p2, p3])           Returns an Array of Values          Returns the Error of the First Failed Promise
