500f83c9255b77e70d49762014cbc5e305ed2842
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-20 21:47:44 +0530

    Refactor --> Added Promises

--- JavaScript/_63_PromiseAPI.js
+++ JavaScript/_63_PromiseAPI.js
@@ -18,3 +18,15 @@ Promise.any([p1, p2, p3])           Returns the Value of the            Returns 
                                     Succeeds, Irrespective of           the promises succeed
                                     Other Failed Promises
 */
+
+const p1=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("P1 Success"),3000)
+});
+
+const p2=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("P2 Success"),1000)
+});
+
+const p3=new Promise((resolve,reject)=>{
+    setTimeout(()=>resolve("P3 Success"),2000)
+});
