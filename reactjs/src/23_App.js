c33845d693acacdaababf9f1d51b6bfb78afa324
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-21 20:53:41 +0530

    Refactor --> Added Routes

--- reactjs/src/23_App.js
+++ reactjs/src/23_App.js
@@ -6,7 +6,9 @@ import LoginPage from './23_pages/Login';
 import MyNavbar from './23_Components/Navbar';
 import ListingPage from './23_pages/List';
 import HomePage from './23_pages/Home';
-import DetailsPage from './23_pages/Details';
+import BookDetailPage from './23_pages/Details';
+import OrdersPage from './23_pages/ViewOrder';
+import ViewOrderDetails from './23_pages/ViewOrderDetails';
 
 
 function App() {
@@ -18,7 +20,9 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/book/list' element={<ListingPage />} />
-        <Route path='/book/view/:bookId' element={<DetailsPage />} />
+        <Route path="/book/view/:bookId" element={<BookDetailPage />} />
+        <Route path="/book/orders" element={<OrdersPage />} />
+        <Route path="/books/orders/:bookId" element={<ViewOrderDetails />} />
       </Routes>
     </div>
   )
