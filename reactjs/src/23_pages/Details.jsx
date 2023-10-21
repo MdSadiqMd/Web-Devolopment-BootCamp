b0d09806eda8704d14d296a54a2e81216707bb15
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-21 20:03:59 +0530

    Refactor --> Added Details Page Content

--- reactjs/src/23_pages/Details.jsx
+++ reactjs/src/23_pages/Details.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Button from 'react-bootstrap/esm/Button';
 import { useFirebase } from '../23_Context/Firebase';
 import { useEffect } from 'react';
 
@@ -28,8 +29,16 @@ function detailsPage() {
         )
     }
   return (
-    <div className="conatiner">
-        
+    <div className="conatiner mt-5">
+        <h1>{data.name}</h1>
+        <img src={url} width="500px" style={{borderRadius:"10px"}} />
+        <h1>Details</h1>
+        <p>Price: Rs.{data.price}</p>
+        <p>ISBN Number: {data.isbn} </p>
+        <p>Owner Details</p>
+        <p>Name: {data.displayName} </p>
+        <p>Email: {data.userEmail} </p>
+        <Button variant='success'>Buy Now</Button>
     </div>
   )
 }
