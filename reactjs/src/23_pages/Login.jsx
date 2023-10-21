01a4115245dbfd4287d0d056dff8bf41a797dcd5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-21 20:47:17 +0530

    Refactor --> Added Login Form

--- reactjs/src/23_pages/Login.jsx
+++ reactjs/src/23_pages/Login.jsx
@@ -2,49 +2,59 @@ import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { useFirebase } from '../23_Context/Firebase';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  const firebaseFunctions = useFirebase();
+  const firebase = useFirebase();
+  const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  // If the user is already logged in, navigate them to the home page
   useEffect(() => {
-    if (firebaseFunctions.isLoggedIn) {
-      return Navigate('/');
+    if (firebase.isLoggedIn) {
+      navigate("/");
     }
-  }, [firebaseFunctions]);
+  }, [firebase, navigate]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const result = await firebaseFunctions.signinUserWithEmailandPassword(email, password);
-      alert(result);
-    } catch (error) {
-      console.error('Error logging in:', error.message);
-    }
+    console.log("login in a user...");
+    const result = await firebase.singinUserWithEmailAndPass(email, password);
+    console.log("Successfull", result);
   };
 
   return (
-    <div className="Container mt-5">
+    <div className="container mt-5">
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+          <Form.Control
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            placeholder="Enter email"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+          <Form.Control
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
+
         <Button variant="primary" type="submit">
           Login
         </Button>
       </Form>
-      <h1 className='mt-05 mb-5'>OR</h1>
-      <button variant='danger' onClick={firebaseFunctions.signinWithGoogle}>Sign In with Google</button>
+      <h1 className="mt-5 mb-5">OR</h1>
+      <Button onClick={firebase.signinWithGoogle} variant="danger">
+        Signin with Google
+      </Button>
     </div>
   );
 };
