34069e59cbf48465906c837af84b1b08480876d6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-22 18:11:46 +0530

    Refactor --> Added Hooks in TypeScript

--- typescript/src/Components/Button.tsx
+++ typescript/src/Components/Button.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Giving props in TypeScript
 interface MyButtonProps {
@@ -6,9 +6,26 @@ interface MyButtonProps {
   onClick?:()=>void; // ***By Giving "?" we make a Prop Optional such that if not it will return error if it is not used
 }
 
+interface Book{
+    name:string;
+    price:number;
+}
+
 const MyButton: React.FC<MyButtonProps> = (props) => {
+  // Hooks in TypeScript (Implicit casting)
+  //const [value,setValue]=useState<number>(0);
+  // Hooks Using Interface (Expicit casting)
+  const [value,setValue]=useState<Book>({
+    name:"One",
+    price:10,
+  });
   return (
-    <button onClick={props.onClick}>{props.text}</button>
+    <>
+    {/*<h1>{value}</h1>
+    <button onClick={() => setValue(value + 1)}>{props.text}</button>*/}
+    <h1>{value.name} and {value.price}</h1>
+    <button onClick={() => setValue({name:"Two",price:20})}>{props.text}</button>
+    </>
     //(or)
     //const {text,onClick}=props;
     //<button onClick={onClick}>{text}</button>
