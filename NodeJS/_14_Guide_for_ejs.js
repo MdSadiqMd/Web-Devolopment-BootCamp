b63282a12063c302d0cb6174289a9bac0b9d5e51
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-23 19:44:36 +0530

    Refactor --> Added Guide for ejs

---
+++ NodeJS/_14_Guide_for_ejs.js
@@ -0,0 +1,18 @@
+// There are Some Steps to be followed When Working with ejs
+// 1. ejs install --> npm i ejs
+// 2. Configure ejs --> app.set("view engine","ejs")
+// 3. Create Folder with name "views"
+// 4. Create ejs Files in "views" Folder
+// 5. and use render function in the main file and write the name of "ejs" file without .ejs  
+
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views","/_12_views"); // Set the "views" directory
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.listen(3000);
