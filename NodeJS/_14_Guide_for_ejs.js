ba0cd1601db6dd42a26d363986680f13acf6571c
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-23 19:51:02 +0530

    Chore --> Added ejs modules

--- NodeJS/_14_Guide_for_ejs.js
+++ NodeJS/_14_Guide_for_ejs.js
@@ -9,7 +9,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views","/_12_views"); // Set the "views" directory
+app.set("views", "C:/Users/Sadiq/Desktop/Web-Development-BootCamp/_14_views"); 
 
 app.get("/", function (req, res) {
   res.render("index");
