fc4e2c5b56ca4bba0f854427c28e7aed3f3d2439
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-28 20:55:08 +0530

    Refactor --> Added React Toastify

--- reactjs/src/24_App.jsx
+++ reactjs/src/24_App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {ToastContainer,toast} from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
+    const notify=()=>{
+        // visit this website "https://fkhadra.github.io/react-toastify/introduction" select parameters and this code will be generated 
+        toast.success('🦄 Wow so easy!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
   return (
-    <div>App</div>
+    <div>
+        <button onClick={notify} >Login</button>
+        <ToastContainer />
+    </div>
   )
 }
 
