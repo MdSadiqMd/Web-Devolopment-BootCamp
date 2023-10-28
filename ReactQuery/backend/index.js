aac6251faa10376a3886bdba15dd4b905ac053ca
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-28 20:44:19 +0530

    Refactor --> Added Product Listing

--- ReactQuery/backend/index.js
+++ ReactQuery/backend/index.js
@@ -1,52 +1,57 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const cors = require('cors'); // Import the cors middleware
+const app = express();
 
-app.get('/api/products',(req,res)=>{
-    const products=[
-        {
-            id:1,
-            name:"A",
-            price:100,
-            image:"https://picsum.images/940/650"
-        }, {
-            id:2,
-            name:"B",
-            price:200,
-            image:"https://picsum.images/940/650"
-        }, {
-            id:3,
-            name:"C",
-            price:300,
-            image:"https://picsum.images/940/650"
-        }, {
-            id:4,
-            name:"D",
-            price:400,
-            image:"https://picsum.images/940/650"
-        }, {
-            id:5,
-            name:"E",
-            price:500,
-            image:"https://picsum.images/940/650"
-        },
-    ]
+app.use(cors());
 
-    // When we perform search the URL will be usually change like "https://localhost:8000/api/products?search=searchedElement"
-    if(req.query.search){
-        const filterProducts=products.filter(product => {
-            product.name.includes(req.query.search)
-        })
-        res.send(filterProducts);
-        return;
-    }
+app.get('/api/products', (req, res) => {
+  const products =  [
+    {
+      id: 1,
+      name: "A",
+      price: 100,
+      image: "https://picsum.images/940/650"
+    },
+    {
+      id: 2,
+      name: "B",
+      price: 200,
+      image: "https://picsum.images/940/650"
+    },
+    {
+      id: 3,
+      name: "C",
+      price: 300,
+      image: "https://picsum.images/940/650"
+    },
+    {
+      id: 4,
+      name: "D",
+      price: 400,
+      image: "https://picsum.images/940/650"
+    },
+    {
+      id: 5,
+      name: "E",
+      price: 500,
+      image: "https://picsum.images/940/650"
+    },
+  ];
 
-    setTimeout(()=>{
-        res.send(products)
-    },3000);
-})
+  if (req.query.search) {
+    const searchQuery = req.query.search.toLowerCase();
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(searchQuery));
+    res.send(filteredProducts);
+    return;
+  }
 
-const port=process.env.PORT || 8000;
+  setTimeout(() => {
+    res.json(products);
+  }, 3000);
+});
 
-app.listen(port,()=>{
-    console.log(`Server Started at ${port}`);
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`);
+});
