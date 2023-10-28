d9e8627274c53896190d270dc74c15ed8d0cf659
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-28 19:31:04 +0530

    Chore --> Added Axios

---
+++ ReactQuery/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [products,setProducts]=useState([]);
+    useEffect(()=>{
+        
+    },[])
+  return (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
