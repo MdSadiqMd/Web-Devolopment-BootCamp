188b55c17bf886ccabf592732a55bd8876bf485f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-30 19:35:21 +0530

    Refactor --> Added react router dom

--- ReactQuery/frontend/src/App.jsx
+++ ReactQuery/frontend/src/App.jsx
@@ -1,57 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react'
+import {} from 'react-router-dom'
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [search, setSearch] = useState('');
-
-  // We shouldn't use async await like this in axios or even in react as it is a hook
-  /*useEffect(async () => {
-      const res = await axios.get('/api/products')
-  }, [])*/
-
-  // To use Async Await we use IIFE
-  // For safety Purpose we use Semicolon before IIFE to differentiate
-  /*useEffect(() => {
-      try {
-          ;(async () => {
-              setLoading(true);
-              setError(false);
-              const response = await axios.get('/api/products');
-              console.log(response.data);
-              setProducts(response.data);
-              setLoading(false);
-          })()
-      } catch (error) {
-          setError(true);
-          setLoading(false);
-      }
-  }, [])*/
-  //(or)
-  useEffect(() => {
-    // To Decrease the Fetching of the api
-    const controller=new AbortController();
-    ;(async () => {
-      const response = await axios.get('/api/products?search=' + search,{
-        signal:controller.signal
-      });
-      console.log(response.data);
-      setProducts(response.data);
-    })();
-
-    //cleanup
-    return()=>{
-        controller.abort();
-    }
-  }, [search]);
-
   return (
-    <div>
-      <h1>Hello World</h1>
-      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-      <h2>Number of Products are {products.length}</h2>
-    </div>
-  );
-};
+    <>
+      <h1>React Query</h1>
+      <div>
+        <h1>Loading</h1>?(
 
-export default App;
+        )
+      </div>
+    </>
+  )
+}
+
+export default App
