c1a548676fd3ee46f9099a7a4e992813ae8f6804
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-31 16:14:25 +0530

    Refactor --> Added Products

--- ReactQuery/frontend/src/App.jsx
+++ ReactQuery/frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Products from './components/Products'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Products />
+    </div>
   )
 }
 
--- ReactQuery/frontend/src/index.js
+++ ReactQuery/frontend/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
