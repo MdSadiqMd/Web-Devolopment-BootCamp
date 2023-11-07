482e0eb8db6803e566b490e22f5d14c46fd7c698
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-07 20:06:22 +0530

    Refactor --> Added Signup page backend

--- nextjs/src/app/signup/page.tsx
+++ nextjs/src/app/signup/page.tsx
@@ -1,25 +1,51 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation"; // from "next Navigation"
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 // "We think" as usual the NextJS will create a route for this but not as the Folder name we give as signup is correct but the file name should be page.tsx but not signup.tsx --> Now renamed
 // testing go on "/signup"
+
+// After signup and got to mongoDB and click on Database --> Browse Collections
 function SignupPage(){
+  const router=useRouter();
   const [user,setUser]=React.useState({
     email:"",
     password:"",
     username:"",
   });
 
-  const onSignup=async()=>{
+  const [buttonDisabled,setButtonDisabled]=React.useState(false);
+  const [loading,setLoading]=React.useState(false);
 
+  useEffect(() => {
+    if(user.email.length>0 && user.password.length>0 && user.username.length>0){
+      setButtonDisabled(false);
+    } else{
+      setButtonDisabled(true);
+    }
+  }, [user])
+  
+  const onSignup=async()=>{
+    try{
+      setLoading(true);
+      const response=await axios.post("/api/users/signup",user);
+      console.log("signup Sucess",response.data);
+      router.push("/login")
+    } catch(error:any){
+      console.log("Signup failed");
+      toast.error(error.message)
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <div>
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>{loading ? "Loading" : "Signup Page"}</h1>
         <hr />
         <label htmlFor="username">username</label>
         <input 
@@ -50,7 +76,7 @@ function SignupPage(){
             />
             <button
             onClick={onSignup}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup Page</button>
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"> {buttonDisabled?"No Signup Fill up the feilds":"Sign up"} </button>
             <Link href="/login">Visit login page</Link>
         </div>
     </div>
