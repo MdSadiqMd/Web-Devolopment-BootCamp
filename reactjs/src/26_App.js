67fc30a0266ee4c6167034524672f187403f9da5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-08 22:39:11 +0530

    Refactor --> Added Custom hook and manipulated the infinite comment section

--- reactjs/src/26_App.js
+++ reactjs/src/26_App.js
@@ -1,16 +1,27 @@
+// App.js
 import React, { useState } from 'react';
 import Comments from './26_Components/comments/comment';
 import './26_App.css';
 import { commentData } from './26_Data/commentData';
+import useCommentHook from './useCommentHook';
 
 const App = () => {
-  const [comments,setComments]=useState(commentData);
-  const handleAddComments=(commentId,comment)=>{
+  const [comments, setComments] = useState(commentData);
+  const { addComment, deleteComment } = useCommentHook();
 
-  }
+  const handleAddComments = (commentId, comment) => {
+    const updatedTree = addComment(comments, commentId, comment);
+    setComments(updatedTree);
+  };
+
+  const handleCommentDelete = (commentId) => {
+    const updatedTree = deleteComment(comments, commentId);
+    setComments(updatedTree);
+  };
+
   return (
     <div>
-      <Comments key={comments.id} comment={commentData} handleAddComments={handleAddComments} />
+      <Comments comment={comments} handleAddComments={handleAddComments} handleCommentDelete={handleCommentDelete} />
     </div>
   );
 }
--- reactjs/src/26_Components/comments/comment.js
+++ reactjs/src/26_Components/comments/comment.js
@@ -1,45 +1,52 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './comment.css';
-import { commentData } from '../../26_Data/commentData';
 
-const Comment = ({ comment }) => {
-    const [showInput,setShowInput]=useState(false);
-    const [commentBody,setCommentBody]=useState("");
+const Comment = ({ comment, handleAddComments, handleCommentDelete }) => {
+  const [showInput, setShowInput] = useState(false);
+  const [commentBody, setCommentBody] = useState("");
 
-    const handleAdd=()=>{
-        let newComments={
-            id:Date.now(),
-            text:commentBody,
-            replies:[]
-        }
-        handleAddComments(comment.id,newComments);
-        setShowInput(false);
-    }
+  const handleAdd = () => {
+    let newComment = {
+      id: Date.now(),
+      text: commentBody,
+      replies: [],
+    };
+    handleAddComments(comment.id, newComment);
+    setShowInput(false);
+  }
+
+  const handleDelete = () => {
+    handleCommentDelete(comment.id);
+  }
+
   return (
     <div>
-      <div className="comment-container">
+      <div className={`${comment.text} && comment-container`}>
         <h3>{comment.text}</h3>
-        {showInput?<input type='text' autoFocus onChange={(e)=>setCommentBody(e.target.value)}/>:<div></div> }
-        {
-            showInput?(
-                <div>
-                    <button onClick={handleAdd}>Add</button>
-                    <button onClick={()=>setShowInput(false)}>Cancel</button>
-                </div>
-            ):(
-                <div>
-                    <button onClick={()=>setShowInput(true)}>Reply</button>
-                    <button>Delete</button>
-                </div>
-            )
-        }
+        {showInput ? <input type='text' autoFocus onChange={(e) => setCommentBody(e.target.value)} /> : null}
+        <div>
+          {showInput ? (
+            <div>
+              <button onClick={handleAdd}>Add</button>
+              <button onClick={() => setShowInput(false)}>Cancel</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => setShowInput(true)}>Reply</button>
+              <button onClick={handleDelete}>Delete</button>
+            </div>
+          )}
+        </div>
       </div>
-      <div style={{paddingLeft:25}} >
-        {
-          comment?.replies?.map((ele) => (
-            <Comment key={ele.id} comment={ele} handleAddComments={handleAddComments} />
-          ))
-        }
+      <div style={{ paddingLeft: 25 }}>
+        {comment.replies && comment.replies.map((ele) => (
+          <Comment
+            key={ele.id}
+            comment={ele}
+            handleAddComments={handleAddComments}
+            handleCommentDelete={handleCommentDelete}
+          />
+        ))}
       </div>
     </div>
   );
