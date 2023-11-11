eea1fa7b8e33df90191dd26d6d0b35def4cd0dc4
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-11 20:40:03 +0530

    Refactor --> Added UI for the signout

--- nextjs/src/app/profile/page.tsx
+++ nextjs/src/app/profile/page.tsx
@@ -1,11 +1,36 @@
-function Profilepage(){
-    return(
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
-            <hr />
-            <h1>Profile Page</h1>
-        </div>
-    )
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation"; 
+
+function ProfilePage() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.get('/api/users/logout');
+      toast.success("Logout Successful");
+      router.push("/login");
+    } catch (error:any) {
+      toast.error(error.message)
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Profile</h1>
+      <hr />
+      <h1>Profile Page</h1>
+      <hr />
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        Log out
+      </button>
+    </div>
+  );
 }
 
-export default Profilepage
+export default ProfilePage;
