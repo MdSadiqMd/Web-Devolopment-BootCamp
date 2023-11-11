e7029e584601a3a56e08900182d84c27ff3d58b9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-11 21:01:29 +0530

    Refactor --> Added Profile Page with data

--- nextjs/src/app/profile/page.tsx
+++ nextjs/src/app/profile/page.tsx
@@ -1,19 +1,32 @@
-"use client"
 import axios from "axios";
 import Link from "next/link";
-import toast from "react-hot-toast";
-import { useRouter } from "next/navigation"; 
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-function ProfilePage() {
+export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
 
   const logout = async () => {
     try {
       await axios.get('/api/users/logout');
-      toast.success("Logout Successful");
-      router.push("/login");
-    } catch (error:any) {
-      toast.error(error.message)
+      toast.success('Logout successful');
+      router.push('/login');
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    try {
+      const res = await axios.get('/api/users/me');
+      console.log(res.data);
+      setData(res.data.data._id);
+    } catch (error) {
+      console.error("Error fetching user details:", error);
+      toast.error("Error fetching user details");
     }
   };
 
@@ -21,16 +34,22 @@ function ProfilePage() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
-      <h1>Profile Page</h1>
-      <hr />
+      <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-500">
+        {data === 'nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}
+      </h2>
       <hr />
       <button
         onClick={logout}
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Log out
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Logout
+      </button>
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        GetUser Details
       </button>
     </div>
   );
 }
-
-export default ProfilePage;
