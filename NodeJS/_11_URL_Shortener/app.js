d0e6cbe6bf2358027ed9eb88e9b34f43327b1f7a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-12 20:35:32 +0530

    Refactor --> Added path of ejs

--- NodeJS/_11_URL_Shortener/app.js
+++ NodeJS/_11_URL_Shortener/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path= require('path');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use(express.static(path.join("C:/Users/Sadiq/Desktop/Web-Devolopment-BootCamp/NodeJS/_11_URL_Shortener","public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/url_shortener', {
