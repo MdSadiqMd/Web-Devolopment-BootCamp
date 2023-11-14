248fe40e654ca0bb098c422236059900a8f614e5
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-14 20:14:09 +0530

    Refactor --> Added index.js for infinite scrolling

---
+++ reactjs/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./28_App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
