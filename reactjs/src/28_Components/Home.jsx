b24f2ac6437cd46c85543a0906c12475c2869b90
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-14 20:35:10 +0530

    Refactor --> Added Home Component

--- reactjs/src/28_Components/Home.jsx
+++ reactjs/src/28_Components/Home.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import MovieComponent from "./MovieComponent";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [card, setCard] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
-export default Home
+  const getCardData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=9&_page=${page}`
+    );
+    const data = await res.json();
+    // Keep the previous cards as it is and add the new cards
+    setCard((prev) => [...prev, ...data]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCardData();
+  }, [page]);
+
+  const handelInfiniteScroll = async () => {
+    // console.log("scrollHeight" + document.documentElement.scrollHeight);
+    // console.log("innerHeight" + window.innerHeight);
+    // console.log("scrollTop" + document.documentElement.scrollTop);
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >=
+        document.documentElement.scrollHeight
+      ) {
+        setLoading(true);
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handelInfiniteScroll);
+    return () => window.removeEventListener("scroll", handelInfiniteScroll);
+  }, []);
+
+  return (
+    <>
+      <MovieComponent movieInfo={card} />
+      {loading && <Loading />}
+    </>
+  );
+};
+
+export default Home;
