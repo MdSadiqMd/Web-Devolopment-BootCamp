14fa17fabf6946bb57977690f49f575e0a072699
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-15 22:18:55 +0530

    Style --> Added page movement styling

--- reactjs/src/29_App.jsx
+++ reactjs/src/29_App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [page,setpage]=useState(1);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     try {
@@ -21,29 +21,44 @@ const App = () => {
     fetchProducts();
   }, []);
 
-  const setPageHandler=(selectedPage)=>{
-    setpage(selectedPage);
-  }
+  const setPageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
 
   return (
-    <div>
+    <div className='container mx-auto my-8'>
       {products.length > 0 && (
-        <div className='grid grid-cols-3 gap-4'>
-          {products.slice(page*10-10/*This is logic to show 10 products sub 1,2,3,.. in page variable */ ,page*10).map((prod) => (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {products.slice(page * 10 - 10, page * 10).map((prod) => (
             <div key={prod.id} className='p-4'>
-              <img src={prod.thumbnail} alt={prod.title} className='w-400 h-400' />
-              <span className='block mt-2'>{prod.title}</span>
+              <img src={prod.thumbnail} alt={prod.title} className='w-full h-auto' />
+              <span className='block mt-2 text-center'>{prod.title}</span>
             </div>
           ))}
         </div>
       )}
-      {products.length>0 && (
-        <div>
-            <span onClick={()=>setpage(page-1)}>Prev</span>
-            {[...Array(products.length/10)].map((_,i)=>{
-                return <span onClick={()=>setPageHandler(i+1)} key={i}>{i+1}</span>
-            })}
-            <span onClick={()=>setpage(page+1)}>Next</span>
+      {products.length > 0 && (
+        <div className='flex justify-center mt-4'>
+          <button
+            onClick={() => setPage(page - 1)}
+            className={`px-4 py-2 border rounded ${
+              page === 1 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-200'
+            }`}
+            disabled={page === 1}>Prev</button>
+          {[...Array(Math.ceil(products.length / 10))].map((_, i) => (
+            <button
+              key={i}
+              onClick={() => setPageHandler(i + 1)}
+              className={`mx-2 px-4 py-2 border rounded ${
+                i + 1 === page ? 'bg-blue-500 text-white' : 'hover:bg-gray-200'
+              }`}>{i + 1}</button>
+          ))}
+          <button
+            onClick={() => setPage(page + 1)}
+            className={`px-4 py-2 border rounded ${
+              page =Math.ceil(products.length / 10) ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-200'
+            }`}
+            disabled={page === Math.ceil(products.length / 10)}>Next</button>
         </div>
       )}
     </div>
