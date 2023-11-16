0885e3b10b88011baec0e0b46173e8cecd93dc2b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-16 21:33:20 +0530

    Refactor --> Added Folder Recursion

--- reactjs/src/30_Components/Folder.js
+++ reactjs/src/30_Components/Folder.js
@@ -1,9 +1,26 @@
-import React from 'react'
+// Folder.js
+import React, { useState } from "react";
 
-const Folder = () => {
-  return (
-    <div>Folder</div>
-  )
-}
+const Folder = ({ explorer }) => {
+  const [expand, setExpand] = useState(false);
 
-export default Folder
+  if (explorer.isFolder) {
+    return (
+      <div className="mt-2.5 ml-2.5">
+        <div onClick={() => setExpand(!expand)}>
+          <span>📁{explorer.name}</span>
+        </div>
+
+        <div className={`ml-5 ${expand ? "block" : "hidden"} ${explorer.items.length > 0 ? "cursor-pointer" : ""}`}>
+          {explorer.items.map((exp) => (
+            <Folder explorer={exp} key={exp.id} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    
+  }
+};
+
+export default Folder;
