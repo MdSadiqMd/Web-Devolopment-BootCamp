5263db319889fa28a8a2500186cdeb87e9f403e3
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-19 16:48:10 +0530

    Refactor --> Added extra reducer

--- reactjs/src/31_redux/slice/todo.js
+++ reactjs/src/31_redux/slice/todo.js
@@ -1,7 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// Action 
+export const fetchTodos = createAsyncThunk('fetchTodos',async () =>{
+    const response=await fetch('https://jsonplaceholder.typicode.com/todos');
+    return response.json();
+})
 
 const todoSlice = createSlice({
     name:'todo',
+    initialState: {
+        isLoading:false,
+        data:null,
+        isError:false,
+    },
+    extraReducers: (builder) =>{
+        builder.addCase(fetchTodos.pending, (state, action) =>{
+            state.isLoading=true;
+        });
+        builder.addCase(fetchTodos.fulfilled, (state, action) =>{
+            state.isLoading=false;
+            state.data=action.payload;
+        });
+        builder.addCase(fetchTodos.rejected, (state, action) =>{
+            state.isError=true;
+            console.log("Error ",action.payload)
+        });
+    }
 })
 
 export default todoSlice.reducer
