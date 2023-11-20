2ebef99a154c0673b4072510da636a4612b93689
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-20 18:13:51 +0530

    Refactor --> Added file that needed to be exported Index.js

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -1,3 +1,7 @@
+// webpack is a bundler which converts our code to the Browser understandable format 
+// here is the javacript code that we have written in out IDE run these two commands for output
+// npm run build --> This will craete a dist folder which contains the bundle files that are been sent to the browser for rendering
+// npm run start --> This will start the webpack server
 const arr=["a","b","c","d","e"];
 console.log(arr);
 export default arr;
