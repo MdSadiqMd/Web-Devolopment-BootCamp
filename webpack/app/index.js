7022c62a48eef00f947cf69513c9831be34e4673
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-20 18:06:18 +0530

    Chore --> Added Webpack Configurations and Installed dependencies

---
+++ webpack/app/index.js
@@ -0,0 +1,3 @@
+const arr=["a","b","c","d","e"];
+console.log(arr);
+export default arr;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,27 +1,28 @@
-const path=require('path');
-const HtmlWebpackPlugin=require('html-webpack-plugin');
-module.exports={
-    entry:"./app/index.js",
-    module:{
-        rules:[
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./app/index.js",
+    module: {
+        rules: [
             {
                 test: /\.svg$/,
-                use:'svg-inline-loader',
+                use: 'svg-inline-loader',
             },
             {
-                test:/\.css$/i,
-                use:["style-loader","css-loader"],
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             },
             {
-                test:/\.(js)$/,
-                use:"babel-loader",
+                test: /\.(js)$/,
+                use: "babel-loader",
             },
         ],
     },
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:"bundle.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
     },
-    plugins:[new HtmlWebpackPlugin()],
-    mode:process.env.NODE_ENV==="production"?"production":"devolopment",
+    plugins: [new HtmlWebpackPlugin()],
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
 };
