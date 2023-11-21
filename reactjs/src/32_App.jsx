cd4142c7a3d1582ac6438940930dd65d1c4fba82
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-21 19:53:10 +0530

    Refactor --> Added Popup Model in React

--- reactjs/src/32_App.jsx
+++ reactjs/src/32_App.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Model from './32_Components/Model';
 
 const App = () => {
+    const [showModel,setShowModel]=useState(false);
+    // This function will decide whether to showPopup or not
+    const handlePopup=()=>{
+        setShowModel(!showModel);
+    }
   return (
     <div className='bg-black text-white'>
       <div className='h-screen flex flex-col items-center gap-6'>
         <h1 className='text-5xl font-bold mt-4'>Popup Modal</h1>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Click Me</button>
+        <button onClick={handlePopup} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Click Me</button>
       </div>
-      <Model />
+      {showModel&&<Model onClose={()=>setShowModel(false)}/>}
     </div>
   );
 }
--- reactjs/src/32_Components/Model.jsx
+++ reactjs/src/32_Components/Model.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { CopyX, Download } from 'lucide-react'
 
-const Model = () => {
+const Model = ({onClose}) => {
+  const modelRef=useRef();
+  // This function makes sure that when we click on any other place other than popup model then the block popup dissappears
+  const closeModel=(e)=>{
+    if(modelRef.current===e.target){
+      onClose();
+    }
+  }
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+    <div ref={modelRef} onClick={closeModel} className='fixed inset-0 bg-black bg-opacity-70 backdrop-blur-sm flex justify-center items-center'>
         <div className='mt-10 flex flex-col gap-5 text-white'>
-            <button className='place-self-end'><CopyX size={30}/></button>
+            <button onClick={onClose} className='place-self-end'><CopyX size={30}/></button>
             <div className='bg-indigo-600 rounded-xl px-20 py-10 flex flex-col gap-5 items-center mx-4'>
                 <h1 className='text-3xl font-extrabold'>Lorem, ipsum dolor.</h1>
                 <p className='text-3xl font-bold max-w-md text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
