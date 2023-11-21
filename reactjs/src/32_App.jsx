e44e153939a2d68cf3c7d10e4a11d6780d9a831e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-21 18:57:55 +0530

    Refactor --> Added Starting Files

---
+++ reactjs/src/32_App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex flex-col items-center gap-6 bg-[#14161b] text-white'>
+        <h1 className='text-5xl font-bold mt-4'>Popup Modal</h1>
+        <button></button>
+    </div>
+  )
+}
+
+export default App
