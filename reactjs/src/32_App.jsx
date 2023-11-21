44fafae25224f183694bd310c3bb6b742b2a1a90
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-21 19:06:41 +0530

    Chore --> Added Lucide react

--- reactjs/src/32_App.jsx
+++ reactjs/src/32_App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Model from './32_Components/Model';
 
 const App = () => {
   return (
-    <div className='h-screen flex flex-col items-center gap-6 bg-[#14161b] text-white'>
+    <div className='bg-black text-white'>
+      <div className='h-screen flex flex-col items-center gap-6'>
         <h1 className='text-5xl font-bold mt-4'>Popup Modal</h1>
-        <button></button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Click Me</button>
+      </div>
+      <Model />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ reactjs/src/32_Components/Model.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CopyX } from 'lucide-react'
+
+const Model = () => {
+  return (
+    <div >
+        Modal
+    </div>
+  )
+}
+
+export default Model
