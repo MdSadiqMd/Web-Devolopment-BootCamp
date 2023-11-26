b5e1b606c0585632052875585832cec29d030e6a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-26 16:54:01 +0530

    Docs --> Added Discord Steps Documnetation

---
+++ NodeJS/_15_Discord_Bot.js
@@ -0,0 +1,13 @@
+// Open Discord and create a Server
+// Go to Settings > Advanced > On the Developer Mode
+// Go to Discord Developer Portal Applications > New Application > Create Application
+// Bot --> select Bot and select Administrator setting
+// OAuth --> URL Generator --> Bot and Administrator
+// At last of the Page you find URL paste it in new tab and authorize it 
+// Again go to Bot and click on Rest Token > Copy it
+
+const {Client,GatewayIntentBits} =require("discord.js");
+
+const client=new Client({
+    intents:[GatewayIntentBits.Guilds] // intends means what permisions we are giving (Hover to go to documentation)
+});
