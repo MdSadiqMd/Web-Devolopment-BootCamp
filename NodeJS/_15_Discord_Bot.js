a1dd88e186e2370e7bf4c786ad5f124deb5844c7
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-26 17:04:43 +0530

    Refactor --> Added Intents enabling

--- NodeJS/_15_Discord_Bot.js
+++ NodeJS/_15_Discord_Bot.js
@@ -4,14 +4,20 @@
 // Bot --> select Bot and select Administrator setting
 // OAuth --> URL Generator --> Bot and Administrator
 // At last of the Page you find URL paste it in new tab and authorize it 
-// Again go to Bot and click on Rest Token > Copy it
+// Again go to Bot and click on Rest Token > Copy it and enable all under privileged gateways
 
 const {Client,GatewayIntentBits, Message} =require("discord.js");
 
 const client=new Client({
-    intents:[GatewayIntentBits.Guilds] // intends means what permisions we are giving (Hover to go to documentation)
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ] // intends means what permisions we are giving (Hover to go to documentation)
 });
 
 client.on("messageCreate",(message)=>{
     console.log(message.content);
-})
+});
+
+client.login('MTE3ODI5MTY2NDU3NDQ5Mjc1Mg.GTUTAK.lY9ew2fWdeIjqdglOKLJWTA4LFdxILtSYYGxzg'); // Token that we have copied
