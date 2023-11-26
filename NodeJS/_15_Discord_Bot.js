d17dac622a355b0843fc61701d689ebdf1b5a66e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-11-26 16:55:38 +0530

    Refactor --> Added Created Message return functioin

--- NodeJS/_15_Discord_Bot.js
+++ NodeJS/_15_Discord_Bot.js
@@ -6,8 +6,12 @@
 // At last of the Page you find URL paste it in new tab and authorize it 
 // Again go to Bot and click on Rest Token > Copy it
 
-const {Client,GatewayIntentBits} =require("discord.js");
+const {Client,GatewayIntentBits, Message} =require("discord.js");
 
 const client=new Client({
     intents:[GatewayIntentBits.Guilds] // intends means what permisions we are giving (Hover to go to documentation)
 });
+
+client.on("messageCreate",(message)=>{
+    console.log(message.content);
+})
