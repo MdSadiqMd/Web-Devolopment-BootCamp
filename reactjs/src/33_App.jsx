b5f008591f61c96939cabb127997a4bddb4aaf31
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-10 19:44:17 +0530

    Docs --> Added Documentation of the problem

--- reactjs/src/33_App.jsx
+++ reactjs/src/33_App.jsx
@@ -1,6 +1,13 @@
 import { useState,useRef,useEffect } from "react";
 
-// 
+// Here we are creating a hook which stores the refernce of the existing states
+// The useWhyDidYouUpate hook takes two arguments one is function and other is props 
+// For testing we took two examples below one is onFn function which always render kkep it aside
+
+// Other is Example Function which increases the count when we pass the Example function in "useWhyDidYouUpate" Function and click on Increment 
+// then as there are prevProps(count:0) is already present as refernece in the hook (useRef()) and further the new prop had came into the function 
+// the function will check whether the prevprop and the current prop is equal or not, if not then the prop is updated so if updated then the updated 
+// prop is added into the whyUpdated empty array and if whyUpdated array length is > 1 it will print the array onto the console
 const useWhyDidYouUpate = (name, props) => {
     const prevProps = useRef();
   
@@ -52,7 +59,7 @@ export default function App() {
     <div className="App">
       <button onClick={() => setCount(count + 1)}>Click me</button>
       <div>{count}</div>
-      <Example fn={onFn} />
+      <Example fn={count} fn1={onFn} />
     </div>
   );
 }
