971ece5534954e0f8531ab803eef3063f6a1b2e2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-12-10 19:30:01 +0530

    Refactor --> Added WhyUpdated Hook

--- reactjs/src/33_App.jsx
+++ reactjs/src/33_App.jsx
@@ -1,4 +1,41 @@
-import { useState } from "react";
+import { useState,useRef,useEffect } from "react";
+
+// 
+const useWhyDidYouUpate = (name, props) => {
+    const prevProps = useRef();
+  
+    useEffect(() => {
+      if (prevProps.current) {
+        const keys = Object.keys({ ...prevProps.current, ...props });
+  
+        const whyUpated = {};
+  
+        keys.forEach((key) => {
+          if (typeof prevProps.current[key] === "object" && props[key] === "object") {
+            if (JSON.stringify(prevProps.current[key]) !==JSON.stringify(props[key])) {
+              whyUpated[key] = {
+                from: prevProps.current[key],
+                to: props[key]
+              };
+            }
+          } else {
+            if (prevProps.current[key] !== props[key]) {
+              whyUpated[key] = {
+                from: prevProps.current[key],
+                to: props[key]
+              };
+            }
+          }
+        });
+  
+        if (Object.keys(whyUpated).length) {
+          console.log("This has caused re-render", whyUpated);
+        }
+      }
+  
+      prevProps.current = props;
+    }, [name, props]);
+};
 
 const Example = (props) => {
     useWhyDidYouUpate("Example", props);
