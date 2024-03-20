b1517be395623d709d2ffe06c8434c1a1786813b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-20 19:13:42 +0530

    refactor added custom template literal

--- JavaScript/_68_Custom_Template_Literal.js
+++ JavaScript/_68_Custom_Template_Literal.js
@@ -1,8 +1,9 @@
 import { readFile } from "fs/promises";
+
 const filePath = new URL("./_68_Custom_Template_Literal.html", import.meta.url);
 let contents = await readFile(filePath, { encoding: "utf-8" });
-const data = { name: "Mohammad Sadiq" };
+const data = { name: "Mohammad Sadiq",age:19 };
 for (const [key, value] of Object.entries(data)) {
   contents = contents.replace(`#{${key}}`, value);
 }
-console.log(contents);
+console.log(contents); // The File Read and replaced the template literal with corresponding Values
