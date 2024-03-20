052431d71850750755f5d5806af4522c741e4434
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-03-20 19:19:11 +0530

    refactor created a custom template Literal

--- JavaScript/_68_Custom_Template_Literal.js
+++ JavaScript/_68_Custom_Template_Literal.js
@@ -1,9 +1,11 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 const filePath = new URL("./_68_Custom_Template_Literal.html", import.meta.url);
 let contents = await readFile(filePath, { encoding: "utf-8" });
-const data = { name: "Mohammad Sadiq",age:19 };
+const data = { name: "Mohammad Sadiq", age: 19 };
 for (const [key, value] of Object.entries(data)) {
   contents = contents.replace(`#{${key}}`, value);
 }
 console.log(contents); // The File Read and replaced the template literal with corresponding Values
+
+await writeFile(new URL("./index.html", import.meta.url), contents); // This will create a new file with name "index.html" and write the data of "_68_Custom_Template_Literal.html" in the new file and it will be write the template Literal
